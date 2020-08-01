943d7de312a7922749dbd5841c6a78ff5f3fa83f
Maciej Jarzębkowski <maciej.jarzebkowski@gmail.com>
2020-08-01 04:58:05 +0200

    add tests

---
+++ Lore-Buissnes-Rules-Service-xUnit-Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lore_Buissnes_Rules_Service_xUnit_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
